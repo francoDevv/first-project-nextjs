00edbb30e592bb3e58f23183a3999022d9e810b0
francoDevv <votta.franco16@gmail.com>
2023-05-17 14:53:56 -0300

    first commit

---
+++ app/services/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServicePage = () => {
+  return (
+    <div>
+        <h1>Service Page</h1>
+        <p>This is the service page</p>
+    </div>
+  )
+}
+
+export default ServicePage
